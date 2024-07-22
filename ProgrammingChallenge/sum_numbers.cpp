f9a1bc8a6b984917bc9e010ad1359b9afce8b36b
wilbur.zheng <freelife365@163.com>
2024-07-22 18:55:20 +0800

    [MDF] get the sum of all numbers between 1 and this input value that are divisible by 3 and 5

--- ProgrammingChallenge/sum_numbers.cpp
+++ ProgrammingChallenge/sum_numbers.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
 
-int main()
-{
+//让用户输入一个值，如何计算1到这个值之间所有可以被3和5整除的自然数的和呢？
+int main() {
+    unsigned int limit;
+    cout << "input you upper limit:";
+    cin >> limit;
+
+    unsigned long long sum = 0;
+    for (unsigned int i = 3; i <= limit; ++i) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            sum += i;
+        }
+    }
+    cout << "The sum is: " << sum << endl;
     return 0;
 }
